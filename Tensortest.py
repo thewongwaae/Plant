f44893c9a1a3d807e10ea50234cbe9665df1fcea
Wong Hong You <wonghongyou@gmail.com>
2023-03-21 21:12:35 +0800

    added Notes

--- Tensortest.py
+++ Tensortest.py
@@ -42,28 +42,20 @@ while(True):
 	filter = [1, 64] # person and potted plant
 
 	arr_labels = classes.numpy().astype('int')[0]
-
-	filtered_indices = [i for i, label_id in enumerate(arr_labels) if label_id in filter]
-	# issue here
-	filtered_labels = [labels[i - 1] for i in filtered_indices]
 	arr_boxes = boxes.numpy()[0].astype('int')
-	filtered_boxes = arr_boxes[filtered_indices]
 	arr_scores = scores.numpy()[0]
-	filtered_scores = arr_scores[filtered_indices]
 
 	img_boxes = rgb
 
-	for score, (ymin, xmin, ymax, xmax), label in zip(filtered_scores, filtered_boxes, filtered_labels):
-		# adjust for how similar an item has to be for it to be categorised
-		if score < 0.5:
+	for score, (ymin,xmin,ymax,xmax), label in zip(arr_scores, arr_boxes, arr_labels):
+		if score < 0.65:
 			continue
-			
-		# display score as percentage
-		score_txt = f'{100 * round(score, 0)}'
-		img_boxes = cv.rectangle(rgb, (xmin, ymax), (xmax, ymin), (0, 255, 0), 1)
+		
+		score_txt = f'{100 * round(score,0)}'
+		img_boxes = cv.rectangle(rgb,(xmin, ymax),(xmax, ymin),(0,255,0),1)      
 		font = cv.FONT_HERSHEY_SIMPLEX
-		cv.putText(img_boxes, label, (xmin, ymax-10), font, 0.5, (255, 0, 0), 1, cv.LINE_AA)
-		cv.putText(img_boxes, label, (xmax, ymax-10), font, 0.5, (255, 0, 0), 1, cv.LINE_AA)
+		cv.putText(img_boxes,label,(xmin, ymax-10), font, 0.5, (255,0,0), 1, cv.LINE_AA)
+		cv.putText(img_boxes,score_txt,(xmax, ymax-10), font, 0.5, (255,0,0), 1, cv.LINE_AA)
 
 	cv.imshow('Output', img_boxes)
 	if cv.waitKey(1) & 0xFF == ord('q'):
---
+++ notes.md
@@ -0,0 +1,20 @@
+# Notes
+Object detection with Tensorflow and OpenCV ~ [Object detection](https://towardsdatascience.com/object-detection-with-tensorflow-model-and-opencv-d839f3e42849)
+Official Tensorflow documentation on image classification and neural networks ~ [TensorFlow image classification](https://www.tensorflow.org/tutorials/images/classification)
+
+# To Do
+- [ ] Backend Python code
+  
+  - [ ] Set up object detection
+	- [ ] Train model to recognise 4 plants
+	  - [ ] Sawi
+	  - [ ] Lady's Finger
+	  - [ ] Water Spinach
+	  - [ ] Coriander
+  
+  - [ ] Train model to classify plant images based on health status
+    - [ ] Convert images to HSL, get saturation and ripeness
+	- [ ] Update a database / API
+
+- [ ] Frontend Flutter application
+  - [ ] Display plant health status
